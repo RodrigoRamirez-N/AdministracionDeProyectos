54a1032b9fa810661388ab34e6dd190a085694f3
AldairZP <jonathan.aldair.zp@gmail.com>
2025-11-07 23:29:45 -0600

    add menu-food pages

---
+++ Proyecto/FrontEnd/CampusVirtual/src/app/modules/equipo4/src/pages/menu-food-desktop/menu-food-desktop.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-menu-food-desktop',
+  standalone: true,
+  imports: [],
+  templateUrl: './menu-food-desktop.html',
+  styleUrl: './menu-food-desktop.css'
+})
+export class MenuFoodDesktop {
+
+}
---
+++ Proyecto/FrontEnd/CampusVirtual/src/app/modules/equipo4/src/pages/menu-food-mobile/menu-food-mobile.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-menu-food-mobile',
+  standalone: true,
+  imports: [],
+  templateUrl: './menu-food-mobile.html',
+  styleUrl: './menu-food-mobile.css'
+})
+export class MenuFoodMobile {
+
+}
