cfc4c6d7e91ae5e3c86f231dade0f5e505a0f2d5
AldairZP <jonathan.aldair.zp@gmail.com>
2025-11-08 23:53:35 -0600

    Refactor main.py to include database initialization on startup

--- Proyecto/Backend/CampusVirtual/main.py
+++ Proyecto/Backend/CampusVirtual/main.py
@@ -1,12 +1,17 @@
 from fastapi import FastAPI
 from Modules.Equipo1.Routes.equipo1_router import router as equipo1_router
-
+from Modules.Equipo4.models.database import create_db_and_tables_equipo4
 
 app = FastAPI()
 
+
+@app.on_event("startup")
+def on_startup() -> None:
+    create_db_and_tables_equipo4()
+
+
 app.include_router(
     equipo1_router,
-    prefix="/api/equipo1", 
-    tags=["Equipo 1 - Items"], 
+    prefix="/api/equipo1",
+    tags=["Equipo 1 - Items"],
 )
-
