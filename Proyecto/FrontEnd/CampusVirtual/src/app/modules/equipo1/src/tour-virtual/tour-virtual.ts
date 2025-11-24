8c86320f2026b48020f842a71baf73f6efb262ae
RodrigoRamirez-N <rodrigo-ramirez@uadec.edu.mx>
2025-11-24 16:06:07 -0600

    Update tour virtual final

--- Proyecto/FrontEnd/CampusVirtual/src/app/modules/equipo1/src/tour-virtual/tour-virtual.ts
+++ Proyecto/FrontEnd/CampusVirtual/src/app/modules/equipo1/src/tour-virtual/tour-virtual.ts
@@ -1,16 +1,36 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-tour-virtual',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './tour-virtual.html',
   styleUrl: './tour-virtual.css'
 })
 export class TourVirtual {
+  currentSlide = 0;
+  slides = [0, 1, 2, 3]; // Array para los indicadores
+
   constructor(private router: Router) {}
 
   volverAHome() {
     this.router.navigate(['/equipo1']);
+  }
+
+  nextSlide() {
+    if (this.currentSlide < 3) {
+      this.currentSlide++;
+    }
+  }
+
+  previousSlide() {
+    if (this.currentSlide > 0) {
+      this.currentSlide--;
+    }
+  }
+
+  goToSlide(index: number) {
+    this.currentSlide = index;
   }
 }
